4775344ca73d52c4c88fe2c3c2ddc23dad959cda
andyptt21 <pat271@ISYS-NC062546.local>
2018-03-28 15:55:29 -0400

    Updating loadOverlapMatrices since overlap matrix names got changed

--- R/GeneralFunctions.R
+++ R/GeneralFunctions.R
@@ -6,6 +6,6 @@ loadOverlapMatrices<- function() {
   load(system.file(package = "RaMP",... = "extdata/genes_overlap_matrix.RData"))
   load(system.file(package = "RaMP",... = "extdata/metabolites_overlap_matrix.RData"))
   load(system.file(package = "RaMP",... = "extdata/analytes_overlap_matrix.RData"))
-  return(list(gene=gene_result,metab=metabolite_result,
+  return(list(gene=genes_result,metab=metabolites_result,
 	analyte=analyte_result))
 }
